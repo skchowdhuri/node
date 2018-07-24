5fb8fadd50ce88a13cc6a6dbd6f6d58653314b36
Sagor Chowdhuri <Sagorchowdhuri@gmail.com>
2018-07-24 11:53:43 +0530

    Comitted

---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,33 @@
+const request=require('request');
+
+
+
+var geoViewer=(address, callback)=>{
+
+    var encodedAddress=encodeURIComponent(address);
+
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        json: true
+    },
+    (error, Response, body)=>{
+        if(body.status==='ZERO_RESULTS'){
+            callback('This is an error message');
+        }
+        else if(body.status==='OK'){
+            callback(undefined,{
+               Address: body.results[0].formatted_address,
+               Latitude: body.results[0].geometry.location.lat,
+                Longitude: body.results[0].geometry.location.lng
+            });
+        }
+        else{
+            console.log('There is a problem at the time of fetching result');
+        }
+    });
+}
+
+
+module.exports={
+    geoViewer: geoViewer
+}
