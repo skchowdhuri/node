69f9f8fba6f4dc17d6401f30697203008bf23cff
Sagor Chowdhuri <sagorchowdhuri@gmail.com>
2018-07-27 00:02:37 +0530

    for server

--- node-web-server/app.js
+++ node-web-server/app.js
@@ -1,9 +1,35 @@
 const express=require('express');
+const hbs=require('hbs');
+const port=process.env.PORT || 3000;
 
 var app=express();
-// app atar jato gulo route ace 3000 port k sunche
-app.get('/', (req,res)=>{
-    res.send('<h2> Here is mhjg </h2>');
+app.set('View Engine','hbs');
+hbs.registerHelper('name','sagor');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.use((req, res, next)=>{
+    console.log('sagor');
+    next();
 });
 
-app.listen(3000);
+app.use(express.static(__dirname+'/public'));
+// app atar jato gulo route ace 3000 port k sunche
+app.get('/', (req,res)=>{
+    //res.send('<h2> Here is mhjg </h2>');
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        message: 'This is paragraph msg',
+        currentYear: new Date().getFullYear()
+    });
+});
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`App is running at port ${port}`)
+});
